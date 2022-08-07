f3c7fd19e7524e9e5d7ac6824deeae510be1b7f3
V21Tymchenko <107572404+V21Tymchenko@users.noreply.github.com>
2022-08-07 18:05:12 +0000

    Deploying to gh-pages from @ V21Tymchenko/command-project@d7e98a5c386b0fc8c9f3736735c2760dbd3043df 🚀

---
+++ index.02b3f8c3.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-buyform-open]"),closeModalBtn:document.querySelector("[data-buyform-close]"),modal:document.querySelector("[data-buyform]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.02b3f8c3.js.map
