9abb522703f0b390a58b08e8a7c82697315389ee
V21Tymchenko <107572404+V21Tymchenko@users.noreply.github.com>
2022-08-07 13:06:13 +0000

    Deploying to gh-pages from @ V21Tymchenko/command-project@110a146a17a06e96a02f2c208e2f0027cb6bb8d3 🚀

---
+++ index.3eb18f16.js
@@ -0,0 +1,2 @@
+var n=document.getElementById("myModal"),t=document.getElementById("myBtn"),e=document.getElementsByClassName("about-us__button")[0];t.onclick=function(){n.style.display="block"},e.onclick=function(){n.style.display="none"},window.onclick=function(t){t.target==n&&(n.style.display="none")};
+//# sourceMappingURL=index.3eb18f16.js.map
---
+++ index.982a7cd4.js
@@ -0,0 +1,2 @@
+!function(){var n=document.getElementById("myModal"),t=document.getElementById("myBtn"),e=document.getElementsByClassName("about-us__button")[0];t.onclick=function(){n.style.display="block"},e.onclick=function(){n.style.display="none"},window.onclick=function(t){t.target==n&&(n.style.display="none")}}();
+//# sourceMappingURL=index.982a7cd4.js.map
