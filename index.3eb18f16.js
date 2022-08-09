87364681c4506c38922f80550cffc70a14ac9a4e
V21Tymchenko <107572404+V21Tymchenko@users.noreply.github.com>
2022-08-09 09:03:19 +0000

    Deploying to gh-pages from @ V21Tymchenko/command-project@9ab983d79cef4804a3e510982ff05d26e19fdb46 🚀

--- index.3eb18f16.js
+++
@@ -1,2 +0,0 @@
-var n=document.getElementById("myModal"),t=document.getElementById("myBtn"),e=document.getElementsByClassName("about-us__button")[0];t.onclick=function(){n.style.display="block"},e.onclick=function(){n.style.display="none"},window.onclick=function(t){t.target==n&&(n.style.display="none")};
-//# sourceMappingURL=index.3eb18f16.js.map
